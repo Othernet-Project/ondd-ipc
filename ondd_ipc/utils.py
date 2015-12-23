595f7d454cb1db1d79f74cf9e889868022fdd11b
Manish Mathai <manishpmathai@gmail.com>
2015-12-23 16:28:47 +0530

    Extract constants to a sub-module

--- ondd_ipc/utils.py
+++ ondd_ipc/utils.py
@@ -1,13 +1,4 @@
-KU_BAND = 'k'
-C_BAND = 'c'
-UNIVERSAL = 'u'
-
-C_OFF = 5150  # Frequency offset for C band
-NA_KU_OFF = 10750  # Frequency offset for North America Ku
-UN_LO_OFF = 9750  # Low band offset
-UN_HI_OFF = 10600  # High band offset
-UN_HI_SW = 11700  # Transponder frequency at which we switch to high band
-
+from .consts import *
 
 def kw2xml(**kwargs):
     """ Convert any keyword parameters to XML
